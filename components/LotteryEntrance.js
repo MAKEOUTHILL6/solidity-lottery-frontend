71d62f27c1d00baf2ddedd992cb5d5484be75022
Martin Atanasov <106104073+MAKEOUTHILL6@users.noreply.github.com>
2022-12-08 22:41:55 +0200

    connecting web3 wallet implemented

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,21 @@
+import { useWeb3Contract } from "react-moralis";
+
+const LotteryEntrance = () => {
+
+    // const {runContractFunction: enterRaffle} = useWeb3Contract({
+    //     abi: //,
+    //     contractAddress: //,
+    //     functionName: //,
+    //     params: {} //,
+    //     msgValue: //,
+
+    // })
+
+    return(
+        <div>
+            Hi from Lottery entrance!
+        </div>
+    )
+}
+
+export default LotteryEntrance;
